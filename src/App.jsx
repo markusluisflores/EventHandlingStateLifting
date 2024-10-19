1530d1c0a42ddd9a875708644eefc2fb44b056da
Markus Luis Flores <markuslsflores@gmail.com>
2024-10-19 17:26:21 -0600

    Lab 4 Solution

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native";
+import ToDoList from "../components/ToDoList";
+import ToDoForm from "./components/ToDoForm";
+
+function App() {
+
+    const [tasks, setTasks] = useState([
+        'Do laundry',
+        'Go to gym',
+        'Walk dog'
+    ])
+
+    const addTask = (taskText) => {
+        setTasks(
+            [...tasks, taskText]
+        );
+    };
+
+    return (
+        <SafeAreaView>
+            <ToDoList tasks={tasks} />
+            <ToDoForm addTask={addTask} />
+        </SafeAreaView>
+    );
+}
+
+export default App;
